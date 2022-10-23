a33dc456f25ea4883e5f6aed822f03d6a5376c6d
Alex250303 <71701264+Alex250303@users.noreply.github.com>
2022-10-23 22:53:06 +0300

    fix integration of prettier with lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,7 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['standard', 'plugin:sonarjs/recommended'],
-  overrides: [],
+  extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
