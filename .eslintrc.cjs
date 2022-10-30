86034c2f120bb11bcf3ebc00f18a34f562520d49
Bogdan6431 <bog.korsun2014@yandex.ru>
2022-10-30 19:38:07 +0200

    fixes package.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
   rules: {
     'n/exports-style': ['error', 'module.exports'],
     'n/file-extension-in-import': ['error', 'always'],
-    'n/prefer-global/buffer': ['error', '“always'],
+    'n/prefer-global/buffer': ['error', 'always'],
     'n/prefer-global/console': ['error', 'always'],
     'n/prefer-global/process': ['error', 'always'],
     'n/prefer-global/url-search-params': ['error', 'always'],
